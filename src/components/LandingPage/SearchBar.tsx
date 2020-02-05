3221a6354c5cdc5d8311429b8ce0161e0d80a6ba
Jakub <jakub.bulhak@gmail.com>
2020-02-05 21:34:54 +0100

    Added wrapper for searchbar.

--- src/components/LandingPage/SearchBar.tsx
+++ src/components/LandingPage/SearchBar.tsx
@@ -16,11 +16,11 @@ const useStyles = makeStyles((theme: Theme) =>
             '&:hover': {
                 backgroundColor: fade(theme.palette.common.white, 0.25),
             },
-            marginRight: theme.spacing(2),
-            marginLeft: 0,
+            // marginRight: theme.spacing(2),
+            // marginLeft: 0,
             width: '100%',
             [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(3),
+                // marginLeft: theme.spacing(3),
                 width: 'auto',
             },
         },
--- src/containers/LandingPage.tsx
+++ src/containers/LandingPage.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import SearchBar from "../components/LandingPage/SearchBar"
+import { Box, Typography } from '@material-ui/core';
+import styled from "styled-components"
+
+const LandingPageWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`
+const HeaderText = styled(Typography)`
+    color: #fff;
+`
+
+const SearchBarWrapper = styled(Box)`
+    width: 60%;
+`
 
 interface IProps {
 
@@ -7,9 +23,14 @@ interface IProps {
 
 const LandingPage: React.FC<IProps> = (props: IProps) => {
     return (
-        <>
-            <SearchBar/>
-        </>
+        <LandingPageWrapper>
+            <SearchBarWrapper component="section">
+                <HeaderText variant="h2" gutterBottom>
+                    Check forecast for your city
+                </HeaderText>
+                <SearchBar/>
+            </SearchBarWrapper>
+        </LandingPageWrapper>
     )
 }
 
